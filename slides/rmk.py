fff7606a3c06bede2969d6c1686762b94bf2cfa2
Dahua Lin <lindahua@gmail.com>
2015-01-03 21:04:57 +0800

    automatic make

--- slides/rmk.py
+++ slides/rmk.py
@@ -1,14 +1,26 @@
 # make slide HTML using remark template
 
+import sys
 from jinja2 import Template
 
 
 if __name__ == '__main__':
 
+	if len(sys.argv) != 2:
+		print """
+Usage:
+
+    python rmk.py <lecture_name>
+
+"""
+		sys.exit(1)
+
+	lec = sys.argv[1]
+
 	tmplfile = "remark_template.html"
-	srcfile = "lec1.md"
-	dstfile = "lec1.html"
-	title = "Lecture 1"
+	srcfile = lec + ".md"
+	dstfile = lec + ".html"
+	title = "MLPI " + lec
 
 	with open(tmplfile, 'r') as f:
 		jtempl = f.read()
